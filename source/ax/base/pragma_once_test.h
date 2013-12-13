307e30f35184de58500a909601e3571ce691ef60
Kirill Berezin <enelar@develop-project.ru>
2013-12-13 10:04:00 +0400

    Pragma once is required to build library

---
+++ source/ax/base/pragma_once_test.h
@@ -0,0 +1,14 @@
+/* This file protected by faith_base_00 license
+* No part of this file could be copyed or changed without Develop Project agreement
+* No part of this file could be used or viewed without Develop Project agreement
+*/
+
+#pragma once
+#ifndef _AX_PRAGMA_ONCE_TEST_H_
+  #define _AX_PRAGMA_ONCE_TEST_H_
+  #ifndef _AX_SKIP_PRAGMA_ONCE_TEST_
+    #include "pragma_once_test.h"
+  #endif
+#else
+  #error #pragma once directive is recomended to build this library. Use _AX_SKIP_PRAGMA_ONCE_TEST_ to workaround
+#endif
